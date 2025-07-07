8add0b6aadb1e27c9b597fcf6551b596ca57a6fc
Darius A. Faorughy <darius.faroughy@rutgers.edu>
2025-07-07 14:59:32 -0700

    update transformers

--- multimodal_flows/networks/ParT.py
+++ multimodal_flows/networks/ParT.py
@@ -34,12 +34,13 @@ class ParticleTransformer(GPT2LMHeadModel):
     def __init__(self, config: GPT2Config, max_time_freq: float = 10.0):
         super().__init__(config)
 
-        # remove the causal mask from Transformer
+        # remove the causal mask from Transformer:
+
         for block in self.transformer.h:
             block.attn.bias.data.fill_(1)
 
-        self.time_emb = TimeFourierEmbedding(config.n_embd, max_freq=max_time_freq)
-
+        self.time_embedding = TimeFourierEmbedding(config.n_embd, max_freq=max_time_freq)
+        self.target_embedding = nn.Embedding(config.vocab_size, config.n_embd)  # k_1
 
     def forward(
         self,
@@ -51,20 +52,19 @@ class ParticleTransformer(GPT2LMHeadModel):
     ):
         """
         Accepts:
-          - input_ids      (B, seq_len)
+          - input_ids      (B, D)
           - time           (B,) or (B,1) with values in [0,1]
-          - attention_mask (B, seq_len)
-          - labels         (B, seq_len), if doing LM training
+          - attention_mask (B, D)
+          - labels         (B, D), if doing LM training
         """
-        inputs_embeds = self.transformer.wte(input_ids)  # (B, seq, D)
-        t_emb = self.time_emb(time)                      # (B, D)
-        B, seq, D = inputs_embeds.size()
-        t_emb = t_emb.unsqueeze(1).expand(B, seq, D)     # (B, seq, D)
-        inputs_embeds = inputs_embeds + t_emb
-        
 
+        inputs_emb = self.transformer.wte(input_ids)  # (B, D, hdim)
+        target_emb = self.target_embedding(labels)    # (B, D, hdim)
+        time_emb = self.time_embedding(time)                # (B, hdim)
+        time_emb = t_emb.unsqueeze(1).expand(*inputs_emb.size())     # (B, D, hdim)
+        
         return super().forward(
-            inputs_embeds=inputs_embeds,
+            inputs_embeds=inputs_emb + target_emb + time_emb,
             attention_mask=attention_mask,
             labels=labels,
             **kwargs,
