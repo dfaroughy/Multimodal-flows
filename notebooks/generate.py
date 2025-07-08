7ba5e0bbb0a9224cabcceb7e15d9e14d2d34d4d4
Darius Faroughy <dariusfar@gmail.com>
2025-07-08 17:43:03 -0400

    update new Jet GPT

--- notebooks/generate.py
+++ notebooks/generate.py
@@ -27,7 +27,7 @@ parser = ArgumentParser()
 parser.add_argument("--dir", type=str, default='/pscratch/sd/d/dfarough')
 parser.add_argument("--predict_type", type=str, default='gen')
 parser.add_argument("--num_nodes", "-N", type=int, default=1)
-parser.add_argument("--project_name", "-proj", type=str, default='tokenized-jets')
+parser.add_argument("--project", "-proj", type=str, default='tokenized-jets')
 parser.add_argument("--experiment_id", "-id", type=str, default=None)
 parser.add_argument("--tag", type=str, default=None)
 parser.add_argument("--data_path", type=str, default='/pscratch/sd/d/dfarough/JetClass')
@@ -42,17 +42,30 @@ parser.add_argument("--plots", "-plt", type=bool, default=False)
 config = parser.parse_args()
 ###############################################################################
 
-model = JetGPT2Model.load_from_checkpoint(f"{config.dir}/{config.project_name}/{config.experiment_id}/checkpoints/{config.checkpoint}", map_location="cpu",)
 
-prompts = torch.full((config.num_jets, 1), model.start_token, dtype=torch.long)
-prompt_dataloadeer = DataLoader(prompts, batch_size=config.batch_size, shuffle=False)
+import torch
+from pytorch_lightning.loggers import CometLogger
+import pytorch_lightning as L
+from torch.utils.data import DataLoader, random_split
+from tensorclass import TensorMultiModal
+from datamodules.datasets import MultiModalDataset, DataCoupling, data_coupling_collate_fn
+from model.multimodal_bridge_matching import MarkovJumpBridge
 
-callback = GeneratorCallback(config=config)
 
-generator = L.Trainer(accelerator="gpu", 
-                      devices='auto', 
-                      strategy='ddp', 
-                      num_nodes=config.num_nodes, 
-                      callbacks=[callback])
+experiment_id = '62ddb0e522d945a7982785b40f7ed96a'
+vocab_size = 9
+num_jets = 3000
+max_num_particles = 150
+batch_size=100
 
-generator.predict(model, dataloaders=prompt_dataloadeer)
+mjb = MarkovJumpBridge.load_from_checkpoint(f"/home/df630/Multimodal-flows/results/jet_sequences/{config.experiment_id}/checkpoints/best.ckpt", map_location="cpu",)
+
+
+noise = torch.randint(0, vocab_size, (num_jets, max_num_particles, 1))
+mask = torch.ones((num_jets, max_num_particles, 1))
+source = TensorMultiModal(discrete=noise, mask=mask)
+
+data = DataCoupling(source=source, target=TensorMultiModal())
+dataset = MultiModalDataset(data)
+predict_dataloader   = DataLoader(dataset, batch_size=batch_size, shuffle=False, collate_fn=data_coupling_collate_fn)
+
