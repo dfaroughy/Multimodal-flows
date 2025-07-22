f65b9dfe4f9d8b33396bc1d83a5d2005424a1437
Darius Faroughy <dariusfar@gmail.com>
2025-07-22 17:49:20 -0400

    registry

--- multimodal_flows/networks/registry.py
+++ multimodal_flows/networks/registry.py
@@ -1,8 +1,9 @@
-
 from networks.ParticleTransformers import ParticleFormer, KinFormer, FlavorFormer
+from networks.BraidedTransformer import MultiModalParticleFormer
 from networks.EPiC import EPiC
 
 MODEL_REGISTRY = {"ParticleFormer": ParticleFormer,
+                  "MultiModalParticleFormer": MultiModalParticleFormer,
                   "KinFormer": KinFormer,
                   "FlavorFormer": FlavorFormer,
                   "EPiC": EPiC
