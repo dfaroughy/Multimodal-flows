29edfc4f35c6204d92101435ec1074fe422807aa
Darius A. Faorughy <darius.faroughy@rutgers.edu>
2025-08-30 10:50:15 -0700

    update

---
+++ scripts/sample_mmf.py
@@ -0,0 +1,172 @@
+import numpy as np
+import torch
+from argparse import ArgumentParser
+import pytorch_lightning as L
+from pytorch_lightning.utilities import rank_zero_only
+import torch.distributed as dist
+from torch.utils.data import DataLoader
+
+from utils.aoj import AspenOpenJets, JetFeatures, sample_from_empirical_masks
+from utils.datasets import MultiModalDataset, DataCoupling, data_coupling_collate_fn
+from utils.callbacks import FlowGeneratorCallback, EMACallback
+from utils.metrics import wasserstein_flavor
+from utils.plotting import plot_flavor_feats, flavor_kinematics, plot_kin_feats
+from utils.tensorclass import TensorMultiModal
+from utils.helpers import load_from_experiment, set_logger
+
+###############################################################################
+
+def experiment_configs():
+    config = ArgumentParser()
+
+    config.add_argument("--num_nodes", "-N", type=int, default=1)
+    config.add_argument("--dir", type=str, default='/pscratch/sd/d/dfarough')
+    config.add_argument("--project", "-proj", type=str, default='jet_sequences')
+    config.add_argument("--experiment_id", "-id", type=str, required=True)
+    config.add_argument("--data_files", "-f", type=str, default='RunG_batch0.h5')
+    config.add_argument("--continuous_features", "-cont", type=str, nargs='*', default=['pt', 'eta_rel', 'phi_rel'])
+    config.add_argument("--discrete_features", "-disc", type=str, default='tokens')
+    config.add_argument("--batch_size", "-bs", type=int, default=256)
+    config.add_argument("--tag", "-t", type=str, default='')
+    config.add_argument("--checkpoint", "-ckpt", type=str, default='best')
+    config.add_argument("--num_jets", "-n", type=int, default=100_000)
+    config.add_argument("--num_timesteps", "-steps", type=int, nargs='*')
+    config.add_argument("--temperature", "-tmp",  type=float, nargs='*')
+    config.add_argument("--top_k", type=int, default=None)
+    config.add_argument("--top_p", type=float, default=None)
+    config.add_argument("--use_final_max_rates", type=bool, default=False)
+    config.add_argument("--num_files", type=int, default=1)
+    config.add_argument("--make_plots", "-plots", type=bool, default=False)
+
+    config = config.parse_args()
+
+    run_config = load_from_experiment(f"{config.dir}/{config.project}/{config.experiment_id}")
+    run_config.continuous_features = config.continuous_features
+    run_config.discrete_features = config.discrete_features
+    run_config.checkpoint = config.checkpoint
+    run_config.data_files = config.data_files
+    run_config.num_jets = config.num_jets
+    run_config.temperature = config.temperature
+    run_config.top_k = config.top_k
+    run_config.top_p = config.top_p
+    run_config.use_final_max_rates = config.use_final_max_rates
+    run_config.num_timesteps = config.num_timesteps
+    run_config.batch_size = config.batch_size
+    run_config.tag = config.tag
+    run_config.num_files = config.num_files
+    run_config.make_plots = config.make_plots
+    
+    return run_config
+
+def _load_pretrained_model(lightning_module, config, temp, num_steps, tag):
+
+    config.tag = f"{tag}_steps_{num_steps}_temp_{temp}"
+    config.res_dir = f"{config.dir}/{config.project}/{config.experiment_id}/generation_results_{config.tag}"
+    ckpt = f"{config.dir}/{config.project}/{config.experiment_id}/checkpoints/{config.checkpoint}.ckpt" 
+    model = lightning_module.load_from_checkpoint(ckpt, map_location="cpu", config=config)
+    model.config.num_timesteps = num_steps 
+    model.config.temperature = temp
+
+    return model
+
+
+def _make_source_dataloader(config):
+
+    aoj = AspenOpenJets(data_dir=config.dir + '/aoj', data_files=config.data_files)
+
+    # target for empirical masks:
+    test, _ = aoj(num_jets=config.num_jets,
+                  download=False,
+                  features={"continuous": config.continuous_features, "discrete": config.discrete_features},    
+                  pt_order=True,
+                  padding='zeros')
+
+    pad_mask = sample_from_empirical_masks(test.mask, config.num_jets, config.max_num_particles)
+    noise_continuous = torch.randn_like(test.continuous) * pad_mask
+    noise_discrete = torch.randint_like(test.discrete, 1, config.vocab_size) * pad_mask
+    t0 = torch.full((len(pad_mask),), config.time_eps)  # (B) t_0=eps
+
+    source = TensorMultiModal(time=t0, continuous=noise_continuous, discrete=noise_discrete,  mask=pad_mask)
+    source_data = DataCoupling(source=source, target=TensorMultiModal())
+    source_dataset = MultiModalDataset(source_data)
+
+    del aoj, test
+
+    return DataLoader(source_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=data_coupling_collate_fn)
+ 
+
+def run_experiment(lightning_module, config, temp, num_steps, tag):
+
+    model = _load_pretrained_model(lightning_module, config, temp, num_steps, tag)
+    
+    callbacks = [FlowGeneratorCallback(config)]
+    
+    if config.use_ema_weights:
+        callbacks += [EMACallback(config)]
+
+    generator = L.Trainer(accelerator="gpu", 
+                          devices='auto',
+                          strategy="ddp",
+                          num_nodes=config.num_nodes, 
+                          callbacks=callbacks,
+                          )
+
+    dataloader = _make_source_dataloader(config)
+    generator.predict(model, dataloaders=dataloader)
+
+    return generator
+
+@rank_zero_only
+def eval_metrics(config, generator):
+    aoj = AspenOpenJets(data_dir=config.dir + '/aoj', data_files="RunG_batch1.h5")
+    test, _ = aoj(num_jets=config.num_jets,
+                  download=False,
+                  features={"continuous": ['pt', 'eta_rel', 'phi_rel'], "discrete": 'tokens'},
+                  pt_order=True,
+                  padding='zeros')
+
+    test = test.squeeze(-1, 'discrete')
+
+    sample = TensorMultiModal.load_from(f"{config.res_dir}/generated_sample.h5")
+    sample = sample.squeeze(-1, 'discrete')
+
+    # metrics & plots:
+
+    fig0 = plot_flavor_feats(sample, test, path=f"{config.res_dir}/plots_flavor.png")
+
+    gen = JetFeatures(sample)
+    aoj = JetFeatures(test) 
+
+    # metrics & plots:
+
+    fig1 = plot_kin_feats(gen, aoj, path=f"{config.res_dir}/plots_kin.png")
+    fig2 = flavor_kinematics(gen, aoj, path=f"{config.res_dir}/flavor_kinematics.png")
+
+    generator.logger.experiment.add_figure('plots_flavor', fig0)
+    generator.logger.experiment.add_figure('plots_kin', fig1)
+    generator.logger.experiment.add_figure('flavor_kinematics', fig2)
+
+
+if __name__ == "__main__":
+
+    from model.MMF import MultiModalFlowBridge
+
+    config = experiment_configs()  
+
+    temperatures = config.temperature   
+    num_steps = config.num_timesteps
+
+    for i in range(0, config.num_files):
+        for temp in temperatures:
+            for step in num_steps:
+                
+                if i > 0: suffix = f'_{i}'
+                else: suffix = ''
+
+                # run_experiment(MultiModalFlowBridge, config, temp, step, tag)
+                generator = run_experiment(MultiModalFlowBridge, config, temp, step, config.tag + suffix)
+
+                if config.make_plots:
+                    if generator.global_rank == 0:
+                        eval_metrics(config, generator)
+                
---
+++ scripts/train_mmf.py
@@ -0,0 +1,187 @@
+import torch
+import numpy as np
+import yaml
+import os
+import pytorch_lightning as L
+import matplotlib.pyplot as plt
+import argparse
+from argparse import ArgumentParser
+from pytorch_lightning.utilities import rank_zero_only
+from torch.utils.data import DataLoader, random_split
+
+from utils.aoj import AspenOpenJets 
+from utils.tensorclass import TensorMultiModal
+from utils.helpers import load_from_experiment, set_logger
+from utils.datasets import MultiModalDataset, DataCoupling, data_coupling_collate_fn
+from utils.callbacks import TrainLoggerCallback, EMACallback, ProgressBarCallback     
+
+
+def experiment_configs():
+
+    config = ArgumentParser()
+
+    # system
+    config.add_argument("--num_nodes", "-N", type=int, default=1)
+    config.add_argument("--dir", type=str, default='/pscratch/sd/d/dfarough')
+    config.add_argument("--project", "-proj", type=str, default='jet_sequences')
+    config.add_argument("--comet_workspace", type=str, default='dfaroughy')
+    config.add_argument("--comet_api_key", type=str, default='8ONjCXJ1ogsqG1UxQzKxYn7tz')
+    config.add_argument("--experiment_id", "-id", type=str, default=None)
+    config.add_argument("--ckpt_path", "-ckpt", type=str, default=None)
+    config.add_argument("--resume_ckpt", "-resume", type=str, default='last') 
+    config.add_argument("--tags", type=str, nargs='*')
+
+    # training
+    config.add_argument("--data_files", "-f", type=str, default='RunG_batch0.h5')
+    config.add_argument("--num_jets", "-n", type=int, default=None)
+    config.add_argument("--max_num_particles", "-d", type=int, default=150)
+    config.add_argument("--batch_size", "-bs", type=int, default=256)
+    config.add_argument("--max_epochs", "-epochs", type=int, default=250)
+    config.add_argument("--train_frac", type=float, default=0.8)
+    config.add_argument("--lr", type=float, default=5e-4)
+    config.add_argument("--lr_final", type=float, default=1e-5)
+    config.add_argument("--warmup_epochs", type=int, default=0)
+    config.add_argument("--use_ema_weights","-ema",  type=bool, default=False)
+    config.add_argument("--ema_decay", type=float, default=0.999)
+
+    # model
+    config.add_argument("--model", "-nn", type=str, default='MultiModalParticleFormer')
+    config.add_argument("--continuous_features", "-cont", type=str, nargs='*', default=['pt', 'eta_rel', 'phi_rel'])
+    config.add_argument("--discrete_features", "-disc", type=str, default='tokens')
+    config.add_argument("--vocab_size", type=int, default=9, help="Number of discrete tokens (8), including a mask token (1)")
+    config.add_argument("--dim_continuous", type=int, default=3)
+    config.add_argument("--n_embd", type=int, default=256)
+    config.add_argument("--n_inner", type=int, default=1024)
+    config.add_argument("--n_layer", type=int, default=2)
+    config.add_argument("--n_layer_fused", type=int, default=2)
+    config.add_argument("--n_head", type=int, default=2)
+    config.add_argument("--dropout", type=float, default=0.0)
+    config.add_argument("--qk_layernorm", type=bool, default=True)
+    config.add_argument("--bias", type=bool, default=True)
+    config.add_argument("--multitask_loss", "-loss", type=str, default='sum')
+    config.add_argument("--use_coocurrence", type=bool, default=False)
+    
+    # dynamics
+    config.add_argument("--gamma", "-gam", type=float, default=0.1)
+    config.add_argument("--sigma", "-sig", type=float, default=1e-5)
+    config.add_argument("--time_eps", "-eps", type=float, default=1e-5)
+
+    # sampling
+    config.add_argument("--num_timesteps", "-steps", type=int, default=100)
+    config.add_argument("--temperature", type=float, default=1.0)
+    config.add_argument("--top_k", type=int, default=None)
+    config.add_argument("--top_p", type=float, default=None)
+
+    config = config.parse_args()
+
+    if config.experiment_id is None: return config
+    else: 
+        path = f"{config.dir}/{config.project}/{config.experiment_id}"
+        run_config = load_from_experiment(path)
+        run_config.max_epochs = config.max_epochs
+        run_config.lr = config.lr
+        run_config.lr_final = config.lr_final
+        run_config.resume_ckpt = config.resume_ckpt
+        return run_config
+
+
+
+def make_dataloaders(config):
+
+    aoj = AspenOpenJets(data_dir=config.dir + '/aoj', data_files=config.data_files)
+
+    # target
+    jets, metadata = aoj(num_jets=config.num_jets,
+                        download=False,
+                        features={"continuous": config.continuous_features, "discrete": config.discrete_features},    
+                        transform='standardize',
+                        pt_order=True,
+                        padding='zeros')
+
+               
+    config.metadata = metadata
+
+    # source
+    noise = TensorMultiModal(mask=jets.mask.clone())
+
+    # source-target coupling
+    data = DataCoupling(source=noise, target=jets)
+    dataset = MultiModalDataset(data)
+    train_size = int(config.train_frac * len(dataset))
+    val_size   = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, collate_fn=data_coupling_collate_fn)
+    val_dataloader   = DataLoader(val_dataset,   batch_size=config.batch_size, shuffle=False, collate_fn=data_coupling_collate_fn)
+ 
+    return train_dataloader, val_dataloader
+
+    
+def run_train_experiment(config, lighting_module: L.LightningModule):
+
+    train, val = make_dataloaders(config)
+
+    if config.experiment_id is not None: 
+        
+        # resume training
+        print('INFO: Resuming training from checkpoint: ', config.resume_ckpt)
+        config.ckpt_path = f"{config.dir}/{config.project}/{config.experiment_id}/checkpoints/{config.resume_ckpt}.ckpt"
+        model = lighting_module.load_from_checkpoint(config.ckpt_path, config=config, map_location="cpu",)
+
+    else: 
+        
+        # new run
+        model = lighting_module(config)
+
+    callbacks = [L.callbacks.ModelCheckpoint(dirpath=None,
+                                            monitor="val_loss",
+                                            filename="best",
+                                            save_top_k=10,
+                                            mode="min",
+                                            save_last=True,
+                                                )]
+    
+    callbacks += [L.callbacks.ModelCheckpoint(dirpath=None,
+                                        monitor="val_loss_mse",
+                                        filename="best_mse",
+                                        save_top_k=10,
+                                        mode="min",
+                                            )]
+
+    callbacks += [L.callbacks.ModelCheckpoint(dirpath=None,
+                                        monitor="val_loss_ce",
+                                        filename="best_ce",
+                                        save_top_k=10,
+                                        mode="min",
+                                            )]
+
+    callbacks += [TrainLoggerCallback(config)]
+    callbacks += [ProgressBarCallback()]
+    
+    if config.use_ema_weights:
+        callbacks += [EMACallback(config)]
+
+
+    logger = set_logger(config)
+
+    trainer = L.Trainer(max_epochs=config.max_epochs, 
+                        accelerator='gpu', 
+                        devices='auto',
+                        strategy='ddp',
+                        num_nodes=config.num_nodes,
+                        callbacks=callbacks,
+                        logger=logger,
+                        sync_batchnorm=True,
+                        gradient_clip_val=1.0,
+                        )
+    
+    trainer.fit(model, train, val, ckpt_path=config.ckpt_path)
+
+
+
+if __name__ == "__main__":
+
+    from model.MMF import MultiModalFlowBridge
+
+    config = experiment_configs()
+    run_train_experiment(config, lighting_module=MultiModalFlowBridge)
+
